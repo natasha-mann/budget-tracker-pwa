62bb7135abdf6efbb1dc4c5f9cf913d96d67310b
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-23 16:20:51 +0100

    updated public path

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 connect();
 
