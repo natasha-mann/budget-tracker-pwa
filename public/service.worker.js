92738691daccab591f0ac938abd8b06dfa05b571
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-23 15:52:12 +0100

    function to install service worker

--- public/service.worker.js
+++ public/service.worker.js
@@ -10,3 +10,13 @@ const urlsToCache = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
