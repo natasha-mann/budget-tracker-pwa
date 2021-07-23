2c2d70c2c01a34b3bca0fd4e6f78b265c8962b92
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-23 15:51:27 +0100

    set up cache

---
+++ public/service.worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
